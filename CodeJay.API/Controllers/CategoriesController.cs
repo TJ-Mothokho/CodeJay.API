855079697dd1f6f19eddc409b9d7e040b25e1008
Tshiamo Justice Mothokho <s225258358@mandela.ac.za>
2024-10-25 10:28:11 +0200

    Added Categories controller

---
+++ CodeJay.API/Controllers/CategoriesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CodeJay.API.Controllers
+{
+    //https://localhost:5001/api/categories
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+    }
+}
