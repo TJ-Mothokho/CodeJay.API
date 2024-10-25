00511e8504fd58c9a0da517766c7e1036ef10b13
Tshiamo Justice Mothokho <s225258358@mandela.ac.za>
2024-10-25 10:25:17 +0200

    Added DbContext

---
+++ CodeJay.DataAccess/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using CodeJay.DataAccess.Models.Domain;
+
+namespace CodeJay.DataAccess.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<BlogPost> BlogPosts { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
