7da7b46ecdd660726fc76d4d64e0d0b1e808a133
Tshiamo Justice Mothokho <s225258358@mandela.ac.za>
2024-10-25 10:30:15 +0200

    Added DTO

---
+++ CodeJay.DataAccess/Models/DTO/CreateCategoryRequestDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeJay.DataAccess.Models.DTO
+{
+    public class CreateCategoryRequestDto
+    {
+        public string Name { get; set; }
+        public string UrlHandle { get; set; }
+    }
+}
